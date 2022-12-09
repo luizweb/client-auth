4a530e8445c67462fad2469e690f2458aea7e20c
Luiz Simoes <luizgsantonio@gmail.com>
2022-12-09 19:09:36 -0300

    alterei protectroute

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:8080",
-  production: "https://dull-gray-turkey-wear.cyclic.app",
+  production: "https://luizweb.cyclic.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
